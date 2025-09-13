061016621a931aae8d238e9aebfd0f3bb151a636
Darito <david01lasso05@gmail.com>
2025-09-13 12:00:26 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -3,22 +3,36 @@ from pygame.math import Vector2
 
 pygame.init()
 
+title_font = pygame.font.Font(None,60)
+
 BG_COLOR = (173, 204, 96)
 SNAKE_COLOR = (43, 51, 24)
 FOOD_COLOR = (222, 68, 35)
 
+cell_size = 30
+number_of_cells = 25
+
+OFFSET = 25
+
 class Food:
-    def __init__(self):
-        self.position = self.generate_random_pos()
+    def __init__(self, snake_body):
+        self.position = self.generate_random_pos(snake_body)
         
     def draw(self):
         food_rect = pygame.Rect(self.position.x * cell_size, self.position.y * cell_size, cell_size, cell_size)
         screen.blit(food_surface, food_rect)
-        
-    def generate_random_pos(self):
+
+    def generate_random_cell(self):
         x = random.randint(0, number_of_cells - 1)
         y = random.randint(0, number_of_cells - 1)
-        position = Vector2(x, y)
+        return Vector2(x, y)
+
+    def generate_random_pos(self, snake_body):
+        position = self.generate_random_cell()
+
+        while position in snake_body:
+            position = self.generate_random_cell()
+
         return position
 
 class Snake:
@@ -29,7 +43,7 @@ class Snake:
     
     def draw(self):
         for segment in self.body:
-            segment_rect = (segment.x * cell_size, segment.y * cell_size, cell_size, cell_size)
+            segment_rect = (OFFSET + segment.x * cell_size,OFFSET + segment.y * cell_size, cell_size, cell_size)
             pygame.draw.rect(screen, SNAKE_COLOR, segment_rect, 0, 7)
             
     def update(self):
@@ -38,29 +52,51 @@ class Snake:
             self.add_segment = False
         else:
             self.body.pop()
+    
+    def reset(self):
+        self.body = [Vector2(6,9), Vector2(5,9), Vector2(4,9)]
+        self.direction = Vector2(1,0)
 
 class Game:
     def __init__(self):
         self.snake = Snake()
-        self.food = Food()
+        self.food = Food(self.snake.body)
+        self.state = "RUNNING"
     
     def draw(self):
         self.snake.draw()
         self.food.draw()
         
     def update(self):
-        self.snake.update()
-        self.check_collision_with_food()
+        if self.state == "RUNNING":
+           self.snake.update()
+           self.check_collision_with_food()
+           self.check_collision_with_edges()
+           self.check_tail_collision()
         
     def check_collision_with_food(self):
         if self.snake.body[0] == self.food.position:
-            self.food.position = self.food.generate_random_pos()
+            self.food.position = self.food.generate_random_pos(self.snake.body)
             self.snake.add_segment = True
 
-cell_size = 30
-number_of_cells = 25
+    def check_collision_with_edges(self):
+        if self.snake.body[0].x == number_of_cells or self.snake.body[0].x == -1:
+           self.game_over()
+        if self.snake.body[0].y == number_of_cells or self.snake.body[0].y == -1:
+           self.game_over()
 
-screen = pygame.display.set_mode((cell_size*number_of_cells, cell_size*number_of_cells))
+    def game_over(self):
+        self.snake.reset()
+        self.food.position = self.food.generate_random_pos(self.snake.body)
+        self.state = "STOPPED"
+    
+    def check_tail_collision(self):
+        headless_body = self.snake.body[1:]
+        if self.snake.body[0] in headless_body:
+            self.game_over()
+
+
+screen = pygame.display.set_mode((2*OFFSET + cell_size*number_of_cells, 2*OFFSET + cell_size*number_of_cells))
 
 pygame.display.set_caption("Retro Snake")
 
@@ -72,7 +108,7 @@ food_surface = pygame.image.load("graphics/food.png")
 
 SNAKE_UPDATE = pygame.USEREVENT
 
-pygame.time.set_timer(SNAKE_UPDATE, 110)
+pygame.time.set_timer(SNAKE_UPDATE,120)
 
 while True:
     for event in pygame.event.get():
@@ -83,6 +119,8 @@ while True:
             game.update()
             
         if event.type == pygame.KEYDOWN:
+            if game.state == "STOPPED":
+                game.state = "RUNNING"
             if event.key == pygame.K_UP and game.snake.direction != Vector2(0,1):
                 game.snake.direction = Vector2(0,-1)
             if event.key == pygame.K_DOWN and game.snake.direction != Vector2(0,-1):
@@ -94,8 +132,11 @@ while True:
     
     #Drawing
     screen.fill(BG_COLOR)
+
+    pygame.draw.rect(screen, SNAKE_COLOR, (OFFSET - 5, OFFSET - 5, cell_size*number_of_cells + 10, cell_size*number_of_cells + 10), 5)
     game.draw()
-    
+    title_surface = title_font.render("Retro Snake", True, SNAKE_COLOR)
+    screen.blit(title_surface, (OFFSET -5, 20))
     
     pygame.display.update()
     clock.tick(60)
